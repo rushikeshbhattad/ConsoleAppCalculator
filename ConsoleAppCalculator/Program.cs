5b65d9684b12db9cef90c66816177b822e077403
Rushikesh Bhattad <rushikeshbhattad8@gmail.com>
2021-08-30 14:51:59 +0530

    Add project files.

---
+++ ConsoleAppCalculator/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Demo3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            int d;
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("xxxx----CALCULATOR-----xxxxx");
+            Console.Write("Enter The Value of A :");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter The Value of B : ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("PRESS ANY Character To Perform Operation  : ");
+            char c = Convert.ToChar(Console.ReadLine());
+
+            switch (c)
+            {
+                case 'A':
+                    d = a + b;
+                    Console.WriteLine("The Addition Value of {0} and {1} = {2} ", a, b, d);
+                    break;
+                case 'S':
+                    d = a - b;
+                    Console.WriteLine("The Subtraction Value of {0} and {1} = {2} ", a, b, d);
+                    break;
+                case 'G':
+                    d = a * b;
+                    Console.WriteLine("The Multiplication Value of {0} and {1} = {2} ", a, b, d);
+                    break;
+                case 'D':
+                    d = a / b;
+                    Console.WriteLine("The Divide Value of {0} and {1} = {2} ", a, b, d);
+                    break;
+                case 'M':
+                    d = a % b;
+                    Console.WriteLine("The Modulous Value of {0} and {1} = {2} ", a, b, d);
+                    break;
+                default:
+                    Console.WriteLine("Instructions");
+                    Console.WriteLine("Press A for Addition");
+                    Console.WriteLine("Press S for Subtraction");
+                    Console.WriteLine("Press D for Divide");
+                    Console.WriteLine("Press G for Multiply");
+                    Console.WriteLine("Press M for Modulous");
+                    break;
+
+            }
+            Console.Write("xxxxxx----CALCULATOR-----xxxxxx");
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
